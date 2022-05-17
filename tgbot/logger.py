db68bea20125dbef984421318bb9010aa4c7a8c7
nikon2283 <abc@steio.ru>
2022-05-17 17:11:34 +0300

    create logger

---
+++ tgbot/logger.py
@@ -0,0 +1,15 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+level = logging.INFO  # logging.DEBUG
+formatter = u'[%(asctime)s] %(filename)s:%(lineno)d #%(levelname)-8s %(message)s'
+date_format = '%H:%M'
+
+logging.basicConfig(
+    level=level,
+    format=formatter,
+    datefmt=date_format,
+)
+
+logger.info("logger started successfully")
