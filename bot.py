5b59eeb153240eaab6b1fb816e118b6658d6a877
nikon2283 <abc@steio.ru>
2022-05-17 17:12:29 +0300

    Init bot

--- bot.py
+++ bot.py
@@ -1,2 +1,39 @@
 # -*- coding: utf-8 -*-
+import asyncio
 
+from aiogram import Bot, Dispatcher
+from aiogram.dispatcher.fsm.storage.memory import MemoryStorage
+
+from tgbot.logger import logger
+from tgbot.config import load_config
+
+from tgbot.handlers import basic_router
+
+
+async def main():
+    logger.info("Starting Bot...")
+
+    # config loader
+    config = load_config(
+        env_path='.env',
+        ini_path='settings.ini',
+    )
+
+    # fsm storage
+    storage = MemoryStorage()
+    bot = Bot(token=config.tg_bot.token, parse_mode=config.misc.parse_mode)
+    dp = Dispatcher(storage=storage)
+
+    for router in [
+        basic_router,
+    ]:
+        dp.include_router(router)
+
+    await dp.start_polling(bot, config=config)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except(KeyboardInterrupt, SystemExit):
+        logger.error("Bot was stopped...")
